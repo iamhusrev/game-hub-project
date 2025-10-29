12a2aef5b6eb8f813490e3b48bed63b61a3d5e9b
iamhusrev <iamhusrev@gmail.com>
2025-10-29 22:26:11 +0300

    feat: refactor GameGrid to use custom hook for game fetching; add useGames hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,30 +1,7 @@
-import { useEffect, useState } from "react";
-import apiClient from "@/services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "@/hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((response) => {
-        setGames(response.data.results);
-      })
-      .catch((err) => {
-        setError("Failed to fetch games" + err.message);
-      });
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <div>
