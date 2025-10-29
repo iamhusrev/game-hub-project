ad4198ed0ac3e5cfe2643b2bf0ec4fd8e7fc85ac
iamhusrev <iamhusrev@gmail.com>
2025-10-29 14:46:38 +0300

    feat: integrate Chakra UI and add color mode toggle functionality
    - Added @chakra-ui/cli as a dev dependency.
    - Refactored App component to include ColorModeButton for theme toggling.
    - Updated NavBar component to include ColorModeButton.
    - Wrapped App component with Provider for Chakra UI and color mode context.
    - Implemented ColorModeProvider and related hooks for managing light/dark themes.
    - Created ColorModeButton component for toggling between light and dark modes.
    - Added Toaster component for displaying notifications.
    - Introduced Tooltip component for enhanced user interface interactions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import { ColorModeButton } from "./components/ui/color-mode";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <NavBar />
       </GridItem>
       <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
-        Aside
+       
       </GridItem>
       <GridItem area="main" bg="dodgerblue">
         Main
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "@/assets/logo.webp";
+import { ColorModeButton } from "./ui/color-mode";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} alt="Game Hub Logo" boxSize="60px" />
       <Text>Game Hub</Text>
+      <ColorModeButton />
     </HStack>
   );
 };
